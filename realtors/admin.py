4c4176f85be200694c144e9b6ad1b51d675a483b
Madaliev Samandar <samandarmadaliev@yandex.com>
2022-06-27 14:10:58 +0500

    initial commit

---
+++ realtors/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from .models import Realtor
+# Register your models here.
+
+class RealtorAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "is_mvp", "email", "hire_date")
+    list_display_links = ("id", "name")
+    search_fields = ("id", "name", "email", "description", "phone")
+    list_editable = ('is_mvp',)
+    list_per_page = 25
+
+
+admin.site.register(Realtor, RealtorAdmin)
